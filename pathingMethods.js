607390c60e8afe05b8a106f590f51c353d037711
Zac Dair <zacdair@gmail.com>
2020-06-26 20:10:56 +0100

    added removeWalls method to remove no viable entries in the cells list

--- pathingMethods.js
+++ pathingMethods.js
@@ -7,8 +7,19 @@ function djikstra(origin, destination, cells){
 
 // Function removes any walls from the list as they aren't viable cells
 function removeWalls(cells){
+    let viableCells = [];
     for (let cell in cells){
-        if(cell
+        //Check if the color section of the cell is white (ie a floor cell)
+        if(cell[0] === "#FFFFFF"){
+            viableCells.push(cell);
+        }
     }
-    return cells;
+    return viableCells;
+}
+
+export function debug(cells){
+    let res = removeWalls(cells);
+    for (let x in res){
+        console.log(x)
+    }
 }
